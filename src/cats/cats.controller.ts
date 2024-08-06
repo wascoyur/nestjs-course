68fce73397fe0658d9a4f5c4ecf89810749efaf9
vasilev,yurii.h3007 <mytem20007@gmail.com>
2024-08-06 19:35:11 +0500

    inner routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -2,18 +2,22 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
+  title = (props: string) => {
+    return `<h3 style="display: grid;height: 100vh; justify-items: center; align-items: center; ">${props}</h3>`;
+  };
+
   @Get()
   findAll(): string {
-    return '<h3 style="display: grid;height: 100vh; justify-items: center; align-items: center; ">Это действие вернет кошек</h3>';
+    return this.title('Это действие вернет кошек');
   }
 
   @Get('breed')
   findBreed(): string {
-    return '<strong style="display: grid;height: 100vh; justify-items: center; align-items: center; ">Это действие вернет breed</strong>';
+    return this.title('Breed is coming');
   }
 
-  @Get('next')
+  @Get('breed/next')
   findInner(): string {
-    return '<strong style="display: grid;height: 100vh; justify-items: center; align-items: center; ">Вложенный роут</strong>';
+    return this.title('Inner route');
   }
 }
